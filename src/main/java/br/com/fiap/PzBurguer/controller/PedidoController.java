4c80bf8220342bd8b2062ff3b69c97246b348809
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-11 21:51:14 -0300

    refatorando e adicionando delete e put

--- src/main/java/br/com/fiap/PzBurguer/controller/PedidoController.java
+++ src/main/java/br/com/fiap/PzBurguer/controller/PedidoController.java
@@ -1,8 +1,12 @@
 package br.com.fiap.PzBurguer.controller;
 
 import br.com.fiap.PzBurguer.model.Pedido;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,31 +15,53 @@ import java.util.List;
 @RequestMapping("/pedido")
 public class PedidoController {
 
+    private Logger log = LoggerFactory.getLogger(getClass());
     List<Pedido> repository = new ArrayList<Pedido>();
 
     @GetMapping()
-    public List<Pedido> getPedidos() {
+    public List<Pedido> index() {
         return repository;
     }
 
-    @PostMapping
+    @GetMapping("/{id}")
+    public Pedido getPedidoById(@PathVariable Long id) {
+        return getPedido(id);
+    }
+
+    @PostMapping()
+    @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<Pedido> create(@RequestBody Pedido pedido) {
-        System.out.println("Criando novo pedido...");
+        log.info("Criando pedido de ID " + pedido.getId());
         repository.add(pedido);
         return ResponseEntity.status(201).body(pedido);
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Pedido> getPedidoById(@PathVariable Long id) {
-        System.out.println("Buscando " + id);
-        var category = repository.stream()
-                .filter(c -> c.getId().equals(id))
-                .findFirst();
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void destroy(@PathVariable Long id) {
+        log.info("Apagando pedido de ID " + id);
+        repository.remove(getPedido(id));
+    }
 
-        if(category.isEmpty()){
-            return ResponseEntity.notFound().build();
-        }
-        return ResponseEntity.ok(category.get());
+    @PutMapping("/{id}")
+    public Pedido update(@PathVariable Long id, @RequestBody Pedido pedido) {
+        log.info("Atualizando o pedido " + id);
+
+        repository.remove(getPedido(id));
+        pedido.setId(id);
+        repository.add(pedido);
+
+        return pedido;
+    }
+
+    private Pedido getPedido(Long id) {
+        return repository
+                .stream()
+                .filter(p -> p.getId().equals(id))
+                .findFirst()
+                .orElseThrow(
+                        () -> new ResponseStatusException(HttpStatus.NOT_FOUND)
+                );
     }
 }
 
