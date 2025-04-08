390eacfe91463cf5f9924d73cdd96c642c6e3ff3
Matheus Esteves <matheus.mat160805@gmail.com>
2025-04-08 11:53:16 -0300

    transformando email em unique

--- src/main/java/br/com/fiap/PzBurguer/model/Usuario.java
+++ src/main/java/br/com/fiap/PzBurguer/model/Usuario.java
@@ -1,9 +1,6 @@
 package br.com.fiap.PzBurguer.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
@@ -20,13 +17,19 @@ public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @NotBlank (message = "O nome deve ser preenchido")
     @Size(min = 3, max = 100, message = "O nome deve ter entre 3 e 100 caracteres")
     private String nome;
+
     @Email
+    @Column(unique = true)
     private String email;
+
     @Pattern(regexp = "^[0-9]{11}$", message = "Telefone inválido")
+    @Column(unique = true)
     private String telefone;
+
     @Size (min = 5, max = 255, message = "A senha deve ter entre 5 e 255 caracteres")
     private String senha;
 
