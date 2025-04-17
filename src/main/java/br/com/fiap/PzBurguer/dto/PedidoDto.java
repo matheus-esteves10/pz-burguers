de00a097a5ab0708694368a136d3aff29ce3aa80
Matheus Esteves <matheus.mat160805@gmail.com>
2025-04-17 10:51:32 -0300

    adicionando o campo de data para o pedido

--- src/main/java/br/com/fiap/PzBurguer/dto/PedidoDto.java
+++ src/main/java/br/com/fiap/PzBurguer/dto/PedidoDto.java
@@ -4,6 +4,7 @@ import br.com.fiap.PzBurguer.model.ItemPedido;
 import br.com.fiap.PzBurguer.model.StatusPedido;
 import br.com.fiap.PzBurguer.model.Usuario;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public record PedidoDto (
@@ -11,6 +12,7 @@ public record PedidoDto (
                          String endereco,
                          String observacoes,
                          Usuario usuario,
+                         LocalDateTime dataPedido,
                          List<ItemPedidoRequest> itens
                          ) {
 }
